ccaa0124067ca902f2a2aec6a2192f7f1597918c
LaramieCode <LaramieCode@gmail.com>
2022-08-25 13:32:19 -0500

    implement event listeners

--- javascript.js
+++ javascript.js
@@ -4,14 +4,14 @@ let gridLayout = []
 const windowWidth = innerWidth
 const gridWidth = (100 / grid) + "%" // 100 represents 100% of the screen and grid is what to divide that by
 
-function createGrid() {   
-    
-    
+function createGrid() {    
 
     for (let i = 0; i < grid * grid; i++) {
         gridLayout[i] = document.createElement("div")
 
         gridLayout[i].style.width = (gridWidth)
+
+        gridLayout[i].addEventListener("mouseover", function() {console.log(`hovered over ${i}`)})
 
         container.appendChild(gridLayout[i])
     }
