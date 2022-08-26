33f725b801cbabcc64f60394ebed6a128e96bbe3
LaramieCode <LaramieCode@gmail.com>
2022-08-26 13:22:07 -0500

    add functional color picker buttons to the menu

--- game.js
+++ game.js
@@ -7,9 +7,22 @@ let gridLayout = []
 let gridWidth = 100 / gridSize + "%"
 let color = "colorBlack"
 
-
-
-
+const black = document.querySelector("#black")
+black.addEventListener("click", function() {colorPicker(colors[0])})
+const white = document.querySelector("#white")
+white.addEventListener("click", function() {colorPicker(colors[1])})
+const orange = document.querySelector("#orange")
+orange.addEventListener("click", function() {colorPicker(colors[2])})
+const yellow = document.querySelector("#yellow")
+yellow.addEventListener("click", function() {colorPicker(colors[3])})
+const red = document.querySelector("#red")
+red.addEventListener("click", function() {colorPicker(colors[4])})
+const green = document.querySelector("#green")
+green.addEventListener("click", function() {colorPicker(colors[5])})
+const blue = document.querySelector("#blue")
+blue.addEventListener("click", function() {colorPicker(colors[6])})
+const purple = document.querySelector("#purple")
+purple.addEventListener("click", function() {colorPicker(colors[7])})
 
 // create grid
 function createGrid() {
@@ -25,6 +38,7 @@ function createGrid() {
 }
 
 function gridPainter(i) {
+    gridLayout[i].className = ""
     gridLayout[i].classList.add(color)
 }
 
