7156af4122e9b4b566f9c3647c3ea4493661b4f6
LaramieCode <LaramieCode@gmail.com>
2022-08-26 12:25:05 -0500

    add ability to paint grid

--- game.js
+++ game.js
@@ -13,11 +13,17 @@ function createGrid() {
     for (let i = 0; i < gridSize * gridSize; i++) {
 
         gridLayout[i] = document.createElement("div")
-
         gridLayout[i].style.width = (gridWidth)
+
+        gridLayout[i].addEventListener("mouseover", function() {gridPainter(i)})
 
         gridContainer.appendChild(gridLayout[i])
     }
+}
+
+function gridPainter(i) {
+    console.log(gridLayout[i])
+    gridLayout[i].classList.add("colorBlack")
 }
 
 createGrid()
